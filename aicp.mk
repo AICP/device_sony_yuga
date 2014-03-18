95665e43f309cff36bcbaf25589a40d128da30b0
Zips <hcolmenares@gmail.com>
2014-03-18 14:44:45 -0400

    yuga: Welcome to AICP     Change-Id: If17037b7676720904e75a4d1b4d642974a1e44b6
    Change-Id: Iee6f3ad1e92a840bb89a1564bee8b72530f06e43

--- aicp.mk
+++ aicp.mk
@@ -1,27 +1,24 @@
 # 
-#  Slim.mk modified by rebelos<ks50497@gmail.com>
 # 
 # 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 # Inherit device configuration
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/aicp/configs/gsm.mk)
 
-# Inherit Slim common Phone stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit AICP common Phone stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := yuga
 
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := slim_yuga
+PRODUCT_NAME := aicp_yuga
 PRODUCT_DEVICE := yuga
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
